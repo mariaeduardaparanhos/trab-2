3310da96c1e78bf9ce627d008f0d18af8a431f92
mariaeduardaparanhos <160735476+mariaeduardaparanhos@users.noreply.github.com>
2024-03-20 10:02:51 -0300

    Add files via upload

---
+++ trabduda.py
@@ -0,0 +1,48 @@
+
+
+numero1 = 10
+numero2 = 20
+numero3 = 30
+numero4 = 40
+
+# uma frase 
+frase= "Duda é linda"
+
+ # armazenamento de uma palavra
+
+palavra= "duda"
+
+# imprimindo os valores armazenados nas variáveis
+print("Número 1:", numero1)
+print("Número 2:", numero2)
+print("Número 3:", numero3)
+print("Número 4:", numero4)
+print("Frase:", frase)
+print("Palavra:", palavra)
+
+numero1 = 2
+numero2 = 45
+numero3 = 78
+numero4 = 32
+
+media = (numero1 + numero2 + numero3 + numero4) / 4
+print ("a media é", media)
+
+quadrado = numero1 + numero1
+print("o quadrado é", quadrado)
+
+dobro = numero2 + 2
+print("o dobro é",dobro)
+
+quantidade_letras = len("duda")
+print("Quantidade de letras da palavra:", quantidade_letras)
+
+quantidade_espaços = frase.count ( "duda é linda")
+print ("quantidade de espaços em branco na frase:", quantidade_espaços)
+
+primeiro_maior = numero1 > numero2
+print( "O primeiro numero é maior que o segundo?", primeiro_maior)
+
+maior_numero = max(numero1, numero2, numero3, numero4)
+print("O maior número é:", maior_numero)
+
